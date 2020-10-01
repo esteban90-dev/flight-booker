d908a587c7c20f3a8ad8288ce5ef3dc905457894
Stephen Allard <stephencallard@gmail.com>
2020-10-01 15:43:39 -0500

    Add airport data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require_relative "airports"
+AIRPORTS = [
+  { code: "ATL",
+    location: "Atlanta" },
+  { code: "PEK",
+    location: "Beijing" },
+  { code: "LAX",
+    location: "Los Angeles" },
+  { code: "DXB",
+    location: "Dubai" },
+  { code: "HND",
+    location: "Tokyo" },
+  { code: "ORD",
+    location: "Chicago" },
+  { code: "LHR",
+    location: "London" },
+  { code: "PVG",
+    location: "Shanghai" },
+  { code: "CDG",
+    location: "Paris" },
+  { code: "DFW",
+    location: "Washington D.C."}
+]
 
 AIRPORTS.each do |airport|
   Airport.create(airport)
