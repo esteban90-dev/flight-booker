ba5bb153cec5884ec7d1727c63b6997e184f8a7b
Stephen Allard <stephencallard@gmail.com>
2020-10-03 14:37:35 -0500

    Rollback database so that flights tables uses datetime after all

--- db/migrate/20201003152448_add_date_add_time_remove_datetime_from_flights.rb
+++
@@ -1,7 +0,0 @@
-class AddDateAddTimeRemoveDatetimeFromFlights < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :flights, :datetime
-    add_column :flights, :date, :date
-    add_column :flights, :time, :time
-  end
-end
