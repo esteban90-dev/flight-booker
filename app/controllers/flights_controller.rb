8dc30b014137215a15df7fd84dfeda327a9363b3
Stephen Allard <stephencallard@gmail.com>
2020-10-03 17:10:50 -0500

    Add search feature with 'date'

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,8 +1,13 @@
 class FlightsController < ApplicationController
   before_action :set_flight, only: [:show]
+  before_action :airport_options, only: [:index]
 
   def index
-    @flights = Flight.all 
+    if params[:search]
+      @flights = Flight.by_date(selected_date)
+    else
+      @flights = Flight.take(5)
+    end
   end
 
   private
@@ -10,4 +15,19 @@ class FlightsController < ApplicationController
   def set_flight
     @flight = Flight.find(params(:id))
   end
+
+  def airport_options
+    @airport_options = Airport.all.map { |airport| [ airport.location, airport.id ] }
+  end
+
+  def search_params
+    params.require(:search).permit(:from_airport, :to_airport, :passenger_number, :date)
+  end
+
+  def selected_date
+    Date.new(search_params["date(1i)"].to_i,
+            search_params["date(2i)"].to_i,
+            search_params["date(3i)"].to_i).beginning_of_day
+  end
+
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,9 @@
 class Flight < ApplicationRecord
   belongs_to :from_airport, class_name: "Airport"
   belongs_to :to_airport, class_name: "Airport"
+
+  def self.by_date(date)
+    date_range = date.beginning_of_day..date.end_of_day
+    where(datetime: date_range)
+  end
 end
