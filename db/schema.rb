7f0b674d6042a116960d923386dcd5e5e327cea6
Stephen Allard <stephencallard@gmail.com>
2020-10-08 21:21:01 -0500

    Add booking model, migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_143751) do
+ActiveRecord::Schema.define(version: 2020_10_09_021130) do
 
   create_table "airports", force: :cascade do |t|
     t.string "code"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2020_10_02_143751) do
     t.string "location"
     t.float "latitude"
     t.float "longitude"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "flight_id"
+    t.integer "passenger_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
+    t.index ["passenger_id"], name: "index_bookings_on_passenger_id"
   end
 
   create_table "flights", force: :cascade do |t|
