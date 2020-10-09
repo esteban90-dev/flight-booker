52a3be171489159006bbc110115ca0bbad697072
Stephen Allard <stephencallard@gmail.com>
2020-10-09 17:56:44 -0500

    Update #new, add #create and #booking_params

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,22 @@
 class BookingsController < ApplicationController
   def new
-    binding.pry
+    @flight = Flight.find(params[:booking][:flight])
+    @booking = Booking.new
+    @booking.passengers.build
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      render plain: "Worked!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [ :id, :name, :email ])
   end
 end
