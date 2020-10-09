79610aa68057cdea248454dd399f9fda8faeacf2
Stephen Allard <stephencallard@gmail.com>
2020-10-09 18:41:29 -0500

    Build # of passengers for a booking depending on params

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,14 +1,18 @@
 class BookingsController < ApplicationController
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
   def new
-    @flight = Flight.find(params[:booking][:flight])
     @booking = Booking.new
-    @booking.passengers.build
+    @booking.flight = Flight.find(params[:booking][:flight])
+    passenger_count.times{ @booking.passengers.build }
   end
 
   def create
     @booking = Booking.new(booking_params)
     if @booking.save
-      render plain: "Worked!"
+      redirect_to booking_path(@booking)
     else
       render :new
     end
@@ -18,5 +22,9 @@ class BookingsController < ApplicationController
 
   def booking_params
     params.require(:booking).permit(:flight_id, passengers_attributes: [ :id, :name, :email ])
+  end
+
+  def passenger_count
+    params[:booking][:passengers].to_i
   end
 end
