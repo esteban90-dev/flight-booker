73ae863b107eedc340b134e8a9c1a950ef0972cb
Stephen Allard <stephencallard@gmail.com>
2020-10-09 17:55:16 -0500

    Update schema to match associations, migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_022141) do
+ActiveRecord::Schema.define(version: 2020_10_09_225015) do
 
   create_table "airports", force: :cascade do |t|
     t.string "code"
@@ -23,11 +23,9 @@ ActiveRecord::Schema.define(version: 2020_10_09_022141) do
 
   create_table "bookings", force: :cascade do |t|
     t.integer "flight_id"
-    t.integer "passenger_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["flight_id"], name: "index_bookings_on_flight_id"
-    t.index ["passenger_id"], name: "index_bookings_on_passenger_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -46,6 +44,7 @@ ActiveRecord::Schema.define(version: 2020_10_09_022141) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "booking_id"
   end
 
 end
