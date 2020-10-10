c17f6d2d6915a411534ce64c5abafda31299c419
Stephen Allard <stephencallard@gmail.com>
2020-10-10 10:46:35 -0500

    Add #find_existing_passenger callback

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,19 @@ class Booking < ApplicationRecord
   has_many :passengers
 
   accepts_nested_attributes_for :passengers
+
+  before_save :find_existing_passengers
+
+  private
+
+  def find_existing_passengers
+    self.passengers = self.passengers.map do |passenger|
+      existing_passenger = Passenger.find_by(name: passenger.name, email: passenger.email)
+      if existing_passenger
+        existing_passenger 
+      else
+        passenger
+      end
+    end
+  end
 end
